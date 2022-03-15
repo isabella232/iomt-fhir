593209a2e31c53eda8eb1b366f13fe4bde472e79
Dustin Burson <duburson@microsoft.com>
2022-03-15 08:42:10 -0700

    * Rename FhirClientValidator to FhirClientValidatorExtensions to match other extension class names * Rename ServiceCollectionExtrensions to FhirClientExtensions to match purpose. * Merge content of HttpClientBuilderRegistraionExtensions into FhirClientExtensions. * Remove registering ITelemetryLogger singleton with FhirClient. ITelemetryLogger is resolved from the service provider now. * Update AddAuthenticationHandler to use the registered singletons for the auth service instead of creating new instances.

--- src/func/Microsoft.Health.Fhir.Ingest.Host/IomtConnectorFunctions.cs
+++ src/func/Microsoft.Health.Fhir.Ingest.Host/IomtConnectorFunctions.cs
@@ -10,13 +10,10 @@ using DevLab.JmesPath;
 using EnsureThat;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Extensions.Options;
 using Microsoft.Health.Common.Telemetry;
 using Microsoft.Health.Expressions;
-using Microsoft.Health.Fhir.Ingest.Data;
 using Microsoft.Health.Fhir.Ingest.Host;
 using Microsoft.Health.Fhir.Ingest.Telemetry;
 using Microsoft.Health.Fhir.Ingest.Template;
@@ -63,48 +60,6 @@ namespace Microsoft.Health.Fhir.Ingest.Service
             {
                 _logger.LogMetric(
                     IomtMetrics.UnhandledException(ex.GetType().Name, ConnectorOperation.FHIRConversion),
-                    1);
-                throw;
-            }
-        }
-
-        [FunctionName("NormalizeDeviceData")]
-        public async Task NormalizeDeviceData(
-            [EventHubTrigger("input", Connection = "InputEventHub")] EventData[] events,
-            [EventHubMeasurementCollector("output", Connection = "OutputEventHub")] IAsyncCollector<IMeasurement> output,
-            [Blob("template/%Template:DeviceContent%", FileAccess.Read)] string templateDefinitions,
-            [DeviceDataNormalization] IOptions<NormalizationServiceOptions> normalizationSettings)
-        {
-            try
-            {
-                EnsureArg.IsNotNull(templateDefinitions, nameof(templateDefinitions));
-                EnsureArg.IsNotNull(events, nameof(events));
-                EnsureArg.IsNotNull(normalizationSettings, nameof(normalizationSettings));
-
-                var templateContext = _collectionContentTemplateFactory.Create(templateDefinitions);
-                templateContext.EnsureValid();
-                var template = templateContext.Template;
-
-                _logger.LogMetric(
-                    IomtMetrics.DeviceEvent(),
-                    events.Length);
-
-                IDataNormalizationService<EventData, IMeasurement> dataNormalizationService = new MeasurementEventNormalizationService(_logger, template, _exceptionTelemetryProcessor);
-                await dataNormalizationService.ProcessAsync(events, output).ConfigureAwait(false);
-
-                if (normalizationSettings.Value.LogDeviceIngressSizeBytes)
-                {
-                    IEventProcessingMeter meter = new EventProcessingMeter();
-                    var eventStats = await meter.CalculateEventStats(events);
-                    _logger.LogMetric(
-                        IomtMetrics.DeviceIngressSizeBytes(),
-                        eventStats.TotalEventsProcessedBytes);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogMetric(
-                    IomtMetrics.UnhandledException(ex.GetType().Name, ConnectorOperation.Normalization),
                     1);
                 throw;
             }
